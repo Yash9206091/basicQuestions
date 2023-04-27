0102dc46a0f6747c2bb979553c272876cff7eeba
Yash9206091 <122716499+Yash9206091@users.noreply.github.com>
2023-04-27 19:37:37 +0530

    Add files via upload

---
+++ same or not.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int a,b;
+	cin>>a>>b;
+	
+	//checking if two no. are same;
+	if(a==b) {
+		cout<<"1";
+	}
+	else {
+		cout<<"0";
+	}
+	return 0;
+}
