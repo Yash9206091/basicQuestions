6c75e09ccc73e8fe426430212b5c864bc05cf7aa
Yash9206091 <122716499+Yash9206091@users.noreply.github.com>
2023-04-27 18:38:05 +0530

    Add files via upload

---
+++ product of 2 numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int x,y;
+	cin>>x>>y;
+	//To print Product of two numbers;
+	int product = x*y;
+	cout<<"The product is : "<<product<<endl;
+}
